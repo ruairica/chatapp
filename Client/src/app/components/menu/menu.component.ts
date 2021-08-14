f1bad78588db763bbd5f1d60e5adfb28bc227ecf
ruairicaldwell <60053464+ruairicaldwell@users.noreply.github.com>
2021-08-14 17:06:58 +0100

    creating android PWA prompt

--- Client/src/app/components/menu/menu.component.ts
+++ Client/src/app/components/menu/menu.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { IChat, IMessage } from 'src/app/data-models/signal-r.types';
 import { MessagingService } from 'src/app/services/messaging.service';
-
+import { Platform } from '@angular/cdk/platform';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -10,15 +10,19 @@ import { MessagingService } from 'src/app/services/messaging.service';
 })
 export class MenuComponent implements OnInit {
 
-  constructor(private router: Router, private messagingService: MessagingService) { }
+  constructor(private router: Router, private messagingService: MessagingService, private platform: Platform) { }
 
   joiningChat = false;
   joinGroupName = '';
   buttonsDisabled = false;
   hasRecentChats = false;
   recentChats: IMessage[] = [];
- 
+  alreadyInstalled = false;
+
   ngOnInit(): void {
+    window.addEventListener('appinstalled', (evt) => {
+      this.alreadyInstalled = true;
+    });
     this.fillRecentMessages();
   }
 
@@ -51,6 +55,15 @@ export class MenuComponent implements OnInit {
       this.messagingService.fillRecentChats(recentChatNames).subscribe((result: IMessage[]) => {
         this.recentChats = result;
         this.hasRecentChats  = true;
+      });
+    }
+  }
+
+  displayDownloadOption(): void {
+    if (this.platform.ANDROID) {
+      window.addEventListener('beforeinstallprompt', (event: any) => {
+        event.preventDefault();
+        event.prompt();
       });
     }
   }
