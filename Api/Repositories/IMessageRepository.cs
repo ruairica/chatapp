4f9d44b7446d84fc10ef17cb3cff97c1cf728c79
ruairicaldwell <60053464+ruairicaldwell@users.noreply.github.com>
2021-08-23 21:19:53 +0100

    refactor cosmos db (#3)
    * added cosmos db DI and created repo class, started migrated db logic to repo
    
    * implemented 24 hour active chat rules.
    
    * started looking at adding emojis
    
    * styling and add validation on join chat

---
+++ Api/Repositories/IMessageRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Api.DataModels;
+
+namespace Api.Repositories
+{
+    public interface IMessageRepository
+    {
+        Task<Chat> CreateNewChat();
+
+        bool ChatExists(string chatName);
+
+        Task<List<MessageResponse>> MessageSummaries(List<string> chatNames);
+
+        Task<List<MessageResponse>> GetMessages(string chatName, double timeStamp);
+
+        Task AddMessage(MessageRequest messageRequest);
+    }
+}
